ab5d5d5d7627c6a82bfe84a256f36818ba7de521
JoonghyunCho <chojoong@gmail.com>
2022-08-09 17:08:03 +0900

    Initial site setup

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Welcome Post",
+        "excerpt":"Welcome to Jay’s Blog.We will see what posts will be updated ;) ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/Welcome/"
+      }]
